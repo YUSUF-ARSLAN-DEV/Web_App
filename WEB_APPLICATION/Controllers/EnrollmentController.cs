90d2ecc913710a41eb1ce3883542ec767866ab9b
Yusuf Arslan <a.online2006@gmail.com>
2026-05-14 17:26:58 +0800

    Fixed some bugs regarding average completion and regarding the completion rate also fixed some navigational problems

--- WEB_APPLICATION/Controllers/EnrollmentController.cs
+++ WEB_APPLICATION/Controllers/EnrollmentController.cs
@@ -12,22 +12,19 @@ namespace WEB_APPLICATION.Controllers
         // gets the list of Enrollments to courses and shows Course Names 
         public ActionResult MyEnrollments()
         {
-            if (Session["userId"] == null)
-                return RedirectToAction("Login", "Account");
-
             int userId = (int)Session["userId"];
-            List<EnrollmentRecord> enrollments = new EnrollmentDAL().GetEnrollmentByUser(userId);
 
-            CourseDAL courseDAL = new CourseDAL();
-            List<Course> courses = new List<Course>();
-            foreach (var enrollment in enrollments)
+            // Force recalculate for all courses
+            var enrollments = new EnrollmentDAL().GetEnrollmentByUser(userId);
+            foreach (var e in enrollments)
             {
-                Course course = courseDAL.GetCourseById(enrollment.courseId);
-                if (course != null)
-                    courses.Add(course);
+                new EnrollmentDAL().RecalculateCompletionRate(userId, e.courseId);
             }
 
-            ViewBag.Courses = courses;
+            // Then get fresh data
+            enrollments = new EnrollmentDAL().GetEnrollmentByUser(userId);
+            ViewBag.Courses = new CourseDAL().GetAllActiveCourses();
+
             return View(enrollments);
         }
 
